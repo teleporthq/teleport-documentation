e5edd8b1cec2a795f957d2a7908bad4612adbac5
Utwo <mihai.legat@gmail.com>
2020-06-19 13:37:32 +0300

    Fix

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     ["link", { rel: "icon", href: "/icons/icon-96x96.png" }],
     ["link", { rel: "manifest", href: "/manifest.json" }],
     ["meta", { name: "theme-color", content: "#822cec" }],
-    ["link", { rel: "apple-touch-icon", href: "/icons/icon-152x152.png" }]
+    ["link", { rel: "apple-touch-icon", href: "/icons/icon-152x152.png" }],
   ],
   title: "teleportHQ Code Generators",
   description:
@@ -13,21 +13,21 @@ module.exports = {
     [
       "@vuepress/pwa",
       {
-        updatePopup: true
-      }
+        updatePopup: true,
+      },
     ],
     [
       "vuepress-plugin-google-tag-manager",
       {
-        gtm: "GTM-5HFFGVX"
-      }
-    ]
+        gtm: "GTM-5HFFGVX",
+      },
+    ],
   ],
   evergreen: true,
   themeConfig: {
     nav: [
       { text: "Try our Playground", link: "https://play.teleporthq.io" },
-      { text: "REPL", link: "https://repl.teleporthq.io" }
+      { text: "REPL", link: "https://repl.teleporthq.io" },
     ],
     logo: "/logo.svg",
     sidebar: [
@@ -40,12 +40,12 @@ module.exports = {
           "guides/generate-your-first-project",
           "guides/customize-your-project-generator",
           "guides/pack-and-publish-your-project",
-          "guides/glossary"
-        ]
+          "guides/glossary",
+        ],
       },
       {
         title: "UIDL",
-        children: ["uidl/", "uidl/structure", "uidl/examples", "uidl/support"]
+        children: ["uidl/", "uidl/structure", "uidl/examples", "uidl/support"],
       },
       {
         title: "Component Generators",
@@ -55,8 +55,8 @@ module.exports = {
           "component-generators/features",
           "component-generators/mappings",
           "component-generators/plugins",
-          "component-generators/post-processors"
-        ]
+          "component-generators/post-processors",
+        ],
       },
       {
         title: "Project Generators",
@@ -67,26 +67,21 @@ module.exports = {
           "project-generators/project-strategy",
           "project-generators/project-template",
           "project-generators/project-packer",
-          "project-generators/publishers"
-        ]
+          "project-generators/publishers",
+        ],
       },
       {
         title: "Community",
         children: [
           "community/",
           "community/contact-us",
-          "community/contributors"
-        ]
+          "community/contributors",
+        ],
       },
       {
         title: "Team",
-        children: [
-          "team/",
-          "team/vision",
-          "team/funding",
-          "team/privacy-policy"
-        ]
-      }
+        children: ["team/", "team/vision"],
+      },
     ],
     sidebarDepth: 3,
     displayAllHeaders: false,
@@ -95,6 +90,6 @@ module.exports = {
     repoLabel: "Contribute on GitHub!",
     docsRepo: "teleporthq/teleport-documentation",
     editLinks: true,
-    editLinkText: "Help us improve this page!"
-  }
+    editLinkText: "Help us improve this page!",
+  },
 };
